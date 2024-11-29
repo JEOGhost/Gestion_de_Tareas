c8e824724fff89274cbb01c16b4784ec9815c2eb
JEOGhost <pinto.99@hotmail.com>
2024-11-28 21:32:55 -0700

    Configuración de la base de datos
    Configuramos SQLite como base de datos y añadimos el código para crear la tabla

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
 
 app = Flask(__name__)
 
+# Inicializador de la base de datos
+@app.route('/initdb')
+def init_db():
+    try:
+        connection = sqlite3.connect('tareas.db')  # Conecta a la base de datos
+        cursor = connection.cursor()
+
+        # Se crea la tabla si no existe
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS tasks (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                title TEXT NOT NULL,
+                start_date TEXT NOT NULL,
+                status TEXT NOT NULL
+            )
+        ''')
+        connection.commit()
+        connection.close()
+        return "Base de datos inicializada."
+    except Exception as e:
+        return f"Error al inicializar: {e}"
+
 @app.route('/')
 def index():
-    return "Flask iniciado."
+    return "Tenemos base de datos."
 
 if __name__ == '__main__':
     app.run(debug=True)
